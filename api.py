dbf69feb2805136dfb120b8e0eb5282ac62a09f9
monroeds <monroeds@rose-hulman.edu>
2024-08-09 21:04:37 -0400

    all done except removing tx api call

--- api.py
+++ api.py
@@ -39,7 +39,7 @@ def cex_checkout():
         pg = 1
         while 1:
             # "https://pro-api.solscan.io/v2.0/account/transfer?address=5tzFkiKscXHK5ZXCGbXZxdw7gTjjD1mBwuoFbhUvuAi9&activity_type[]=ACTIVITY_SPL_TRANSFER&token=So11111111111111111111111111111111111111111&amount[]=5&block_time[]=123&block_time[]=165441&flow=out&page=1&page_size=10"
-            api_call = "https://pro-api.solscan.io/v2.0/account/transfer?address=" + wallet + "&activity_type[]=" + CEX_ACTIVITY_TYPE + "&token=" + NATIVE_SOLANA + "&amount[]=5&block_time[]=" + str((datetime.now() - timedelta(hours=12)).timestamp()) + "&block_time[]=" + str((datetime.now()).timestamp()) + "flow=out&page=" + str(pg) + "&page_size=" + str(CEX_PG_SZ)
+            api_call = "https://pro-api.solscan.io/v2.0/account/transfer?address=" + wallet + "&activity_type[]=" + CEX_ACTIVITY_TYPE + "&token=" + NATIVE_SOLANA + "&amount[]=10&block_time[]=" + str((datetime.now() - timedelta(hours=12)).timestamp()) + "&block_time[]=" + str((datetime.now()).timestamp()) + "flow=out&page=" + str(pg) + "&page_size=" + str(CEX_PG_SZ)
             pg += 1
             response = requests.get(api_call, headers=headers)
             if not response:
@@ -56,7 +56,6 @@ def cex_checkout():
                 if transfer["amount"] / 10 ** transfer["token_decimals"] > 5:
                     print("checking out wallet")
                     wallet_checkout(transfer["to_address"], transfer["block_time"])
-    print("list so far: " + potential_insider_tokens)
     return
 
 # analyze the transfers of wallet until the desired block_time is reached
@@ -64,38 +63,38 @@ def wallet_checkout(wallet, check_until):
     pg = 1
     flag = 1
     viewed_tokens = {}
-    while flag:
-        api_call = "https://pro-api.solscan.io/v2.0/account/transfer?address=" + wallet + "&activity_type[]=" + DEX_ACTIVITY_TYPE + "&token=" + NATIVE_SOLANA + "&amount[]=4.9&block_time[]=" + str(check_until) + "&block_time[]=" + str((datetime.now()).timestamp()) +"&flow=out&page=" + str(pg) + "&page_size=" + str(DEX_PG_SZ)
-        pg += 1
-        response = requests.get(api_call, headers=headers)
-        if not response:
-            raise Exception(f"Non-success status code: {response.status_code}")
-        data = response.json()["data"]
-        if len(data) == 0:
-            break
-        for transfer in data:
-            big_buy = 0
-            transaction_id = transfer["trans_id"]
-            api_tx_call = "https://pro-api.solscan.io/v2.0/transaction/actions?tx=" + transaction_id
-            tx_response = requests.get(api_tx_call, headers=headers)
-            if not tx_response:
-                raise Exception(f"Non-success status code: {tx_response.status_code}")
-            tx = tx_response.json()["data"]
-            if tx["block_time"] > check_until:
-                swaps = tx["transfers"]
-                for swap in swaps:
-                    if swap["transfer_type"] == DEX_ACTIVITY_TYPE:
-                        if swap["token_address"] == NATIVE_SOLANA:
-                            if swap["amount"] / 10 ** swap["decimals"] > 4.9:
-                                big_buy = 1
-                        elif big_buy == 1:
-                            # made a big buy, now we're in the swap for the potential insider token     
-                            if swap["token_address"] not in ignore and viewed_tokens.get(swap["token_address"], 0) == 0:
-                                potential_insider_tokens[swap["token_address"]] = potential_insider_tokens.get(swap["token_address"], 0) + 1
-                                viewed_tokens[swap["token_address"]] = viewed_tokens.get(swap["token_address"], 0) + 1
-            else:
-                flag = 0
-                break
+    # while flag:
+    api_call = "https://pro-api.solscan.io/v2.0/account/transfer?address=" + wallet + "&activity_type[]=" + DEX_ACTIVITY_TYPE + "&token=" + NATIVE_SOLANA + "&amount[]=9.9&block_time[]=" + str(check_until) + "&block_time[]=" + str((datetime.now()).timestamp()) +"&flow=out&page=" + str(pg) + "&page_size=" + str(DEX_PG_SZ)
+    # pg += 1
+    response = requests.get(api_call, headers=headers)
+    if not response:
+        raise Exception(f"Non-success status code: {response.status_code}")
+    data = response.json()["data"]
+        # if len(data) == 0:
+        #     break
+        # for transfer in data:
+        #     big_buy = 0
+        #     transaction_id = transfer["trans_id"]
+        #     api_tx_call = "https://pro-api.solscan.io/v2.0/transaction/actions?tx=" + transaction_id
+        #     tx_response = requests.get(api_tx_call, headers=headers)
+        #     if not tx_response:
+        #         raise Exception(f"Non-success status code: {tx_response.status_code}")
+        #     tx = tx_response.json()["data"]
+        #     if tx["block_time"] > check_until:
+        #         swaps = tx["transfers"]
+        #         for swap in swaps:
+        #             if swap["transfer_type"] == DEX_ACTIVITY_TYPE:
+        #                 if swap["token_address"] == NATIVE_SOLANA:
+        #                     if swap["amount"] / 10 ** swap["decimals"] > 4.9:
+        #                         big_buy = 1
+        #                 elif big_buy == 1:
+        #                     # made a big buy, now we're in the swap for the potential insider token     
+        #                     if swap["token_address"] not in ignore and viewed_tokens.get(swap["token_address"], 0) == 0:
+        #                         potential_insider_tokens[swap["token_address"]] = potential_insider_tokens.get(swap["token_address"], 0) + 1
+        #                         viewed_tokens[swap["token_address"]] = viewed_tokens.get(swap["token_address"], 0) + 1
+        #     else:
+        #         flag = 0
+        #         break
     return
 
 if __name__ == "__main__":
